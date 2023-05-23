952e972c7313590667f00b77805f6db859b4d07f
0xCryptoAngel <bryanballidev@gmail.com>
2023-05-24 06:02:13 +0900

    add mongoose configuration

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const dbConfig = async () => {
+  mongoose.set("strictQuery", false);
   await mongoose.connect(process.env.MONGO_URI!);
   console.log("MongoDb Connected");
 };
